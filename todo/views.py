b2fc18447ea86757b8b254cc1c61511d97c5f064
jatoad <jasperhmca.todd@gmail.com>
2023-06-16 14:36:23 +0000

    wrote tests for forms models and views to check javascript code is running correctly

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_item_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Item
 from .forms import ItemForm
 
